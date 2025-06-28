a389d5d62513cda756bdcea0ae5cc46390ce38fa
Uday Ige <udayige1696@gmail.com>
2025-06-28 12:48:32 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = ({ width = '70px' }) => {
   return (
     <div className='flex items-center justify-center'>
       <img
-        src="../../public/Logo.svg"
+        src="/Logo.svg"
         alt="Logo"
         style={{ width: width }}
       />
